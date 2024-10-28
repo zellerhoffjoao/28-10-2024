47b669d13b28f355d30a66331ccc26377c610ddb
zellerhoffjoao <143816700+zellerhoffjoao@users.noreply.github.com>
2024-10-28 11:59:50 -0300

    Futebol

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao( 
+'Futebol',
+'Qual é o melhor jogador da seleção brasileira nos últimos 15 anos?',
+'Neymar'
+)
+
+
+
+CriaCartao(
+    'Futebol',
+    'Qual é o maior artilheiro da seleção brasileira?',
+    'Neymar'
+)
+
+
+
+CriaCartao(
+    'Futebol',
+    'Qual é o melhor jogador do mundo pós Pelé?',
+    'Cristiano Ronaldo',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+
+
+criaCartao(
+    'Futebol',
+    'Qual é o maior artilheiro da história com números oficiais?',
+    'Cristiano Ronaldo'
+)
